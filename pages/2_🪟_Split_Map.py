96668b82ef10cde002e1ba63eb7e78208f458cc9
Bryan77778 <s1143018@gm.ncue.edu.tw>
2024-11-28 21:29:12 +0800

    Update 2_🪟_Split_Map.py

--- pages/2_🪟_Split_Map.py
+++ pages/2_🪟_Split_Map.py
@@ -13,7 +13,7 @@ st.sidebar.info(markdown)
 logo = "https://i.imgur.com/UbOXYAU.png"
 st.sidebar.image(logo)
 
-st.title("Split-panel Map")
+st.title("Split-panel Map(尚未成功)")
 
 with st.expander("See source code"):
     with st.echo():
