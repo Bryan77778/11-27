dffaa8147424625255e8eb00a4deb7cd9aed78b9
Bryan77778 <s1143018@gm.ncue.edu.tw>
2024-11-28 21:25:10 +0800

    Update 1_🌍_Interactive_Map.py

--- pages/1_🌍_Interactive_Map.py
+++ pages/1_🌍_Interactive_Map.py
@@ -11,7 +11,6 @@ st.sidebar.info(markdown)
 logo = "https://i.imgur.com/UbOXYAU.png"
 st.sidebar.image(logo)
 
-
 st.title("Interactive Map")
 
 col1, col2 = st.columns([4, 1])
@@ -19,14 +18,31 @@ options = list(leafmap.basemaps.keys())
 index = options.index("OpenTopoMap")
 
 with col2:
-
     basemap = st.selectbox("Select a basemap:", options, index)
 
-
 with col1:
-
     m = leafmap.Map(
         locate_control=True, latlon_control=True, draw_export=True, minimap_control=True
     )
     m.add_basemap(basemap)
+
+    # 資料來源
+    water_quality_stations_url = "https://github.com/Bryan77778/11-27/raw/refs/heads/main/%E6%B5%B7%E5%9F%9F%E6%B0%B4%E8%B3%AA%E6%B8%AC%E7%AB%99.geojson"
+    fishing_spots_url = "https://github.com/Bryan77778/11-27/raw/refs/heads/main/%E5%85%A8%E5%8F%B0%E9%96%8B%E6%94%BE%E9%87%A3%E9%BB%9E%E4%BD%8D%E7%BD%AE%20(1).geojson"
+
+    # 添加水質測站圖層（藍色）
+    m.add_geojson(
+        water_quality_stations_url,
+        layer_name="Water Quality Stations",
+        style={"color": "blue", "weight": 2, "fillColor": "blue", "fillOpacity": 0.5},
+    )
+
+    # 添加釣點圖層（紅色）
+    m.add_geojson(
+        fishing_spots_url,
+        layer_name="Fishing Spots",
+        style={"color": "red", "weight": 2, "fillColor": "red", "fillOpacity": 0.5},
+    )
+
+    # 在 Streamlit 中顯示地圖
     m.to_streamlit(height=700)
