0e8c417bad7c2aeafb2251fe8cdca33760160591
Bryan77778 <s1143018@gm.ncue.edu.tw>
2024-12-11 15:40:52 +0800

    Update 2_🪟_Split_Map.py

--- pages/2_🪟_Split_Map.py
+++ pages/2_🪟_Split_Map.py
@@ -20,13 +20,13 @@ with st.expander("See source code"):
         water_quality_stations_url = "https://github.com/Bryan77778/11-27/raw/refs/heads/main/%E6%B5%B7%E5%9F%9F%E6%B0%B4%E8%B3%AA%E6%B8%AC%E7%AB%99.geojson"
         fishing_spots_url = "https://github.com/Bryan77778/11-27/raw/refs/heads/main/%E5%85%A8%E5%8F%B0%E9%96%8B%E6%94%BE%E9%87%A3%E9%BB%9E%E4%BD%8D%E7%BD%AE%20(1).geojson"
 
-        left_map = leafmap.Map(center=[23.5, 121], zoom=20) 
+        left_map = leafmap.Map(center=[23.5, 121], zoom=8) 
         left_map.add_geojson(
             water_quality_stations_url,
             layer_name="Water Quality Stations",
         )
             
-        right_map = leafmap.Map(center=[23.5, 121], zoom=10)
+        right_map = leafmap.Map(center=[23.5, 121], zoom=8)
         right_map.add_geojson(
             fishing_spots_url,
             layer_name="Fishing Spots",
@@ -36,8 +36,8 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.write("### 左側地圖：水質測站")
-    left_map.to_streamlit(height=700)
+    left_map.to_streamlit(height=600)
 
 with col2:
     st.write("### 右側地圖：釣魚點")
-    right_map.to_streamlit(height=700)
+    right_map.to_streamlit(height=600)
