063a358a9d81fd53a1dc455f61c75b97183050b8
Bryan77778 <s1143018@gm.ncue.edu.tw>
2024-12-12 22:52:51 +0800

    Update 📍_Marker_Cluster.py

--- pages/📍_Marker_Cluster.py
+++ pages/📍_Marker_Cluster.py
@@ -26,7 +26,10 @@ with col2:
 with col1:
     options = list(leafmap.basemaps.keys())
     basemap = st.selectbox("選擇第一張地圖底圖:", options, index=options.index("OpenTopoMap"))
-    
+
+    water_quality_stations_url = "https://github.com/Bryan77778/11-27/raw/refs/heads/main/%E6%B5%B7%E5%9F%9F%E6%B0%B4%E8%B3%AA%E6%B8%AC%E7%AB%99.geojson"
+    fishing_spots_url = "https://github.com/Bryan77778/11-27/raw/refs/heads/main/%E5%85%A8%E5%8F%B0%E9%96%8B%E6%94%BE%E9%87%A3%E9%BB%9E%E4%BD%8D%E7%BD%AE%20(1).geojson"
+
     m1 = leafmap.Map(center=[25, 121], zoom=7)
     m1.add_basemap(basemap)
     
