fe711ae9ca9f65b276bdd0eb9b5b53d32f0a315f
Bryan77778 <s1143018@gm.ncue.edu.tw>
2024-12-15 20:45:08 +0800

    Update 2_點位雙地圖顯示.py

--- pages/2_點位雙地圖顯示.py
+++ pages/2_點位雙地圖顯示.py
@@ -29,7 +29,7 @@ row1_col1, row1_col2 = st.columns(2)
 
 with row1_col1:
     basemap1 = st.selectbox("選擇左側地圖的底圖:", basemap_options, index=basemap_options.index("OpenStreetMap"))
-    st.write("#### 左側地圖：水質測站 (Marker Cluster)")
+    st.write("#### 左側地圖：水質測站 (點位)")
     m1 = leafmap.Map(center=[23.5, 121], zoom=8)
     m1.add_basemap(basemap1)
     m1.add_geojson(water_quality_stations_url, layer_name="Water Quality Stations")
@@ -37,7 +37,7 @@ with row1_col1:
 
 with row1_col2:
     basemap2 = st.selectbox("選擇右側地圖的底圖:", basemap_options, index=basemap_options.index("OpenStreetMap"))
-    st.write("#### 右側地圖：釣魚點 (Marker Cluster)")
+    st.write("#### 右側地圖：釣魚點 (點位)")
     m2 = leafmap.Map(center=[23.5, 121], zoom=8)
     m2.add_basemap(basemap2)
     m2.add_geojson(fishing_spots_url, layer_name="Fishing Spots")
@@ -58,7 +58,7 @@ except Exception as e:
 
 with row2_col1:
     basemap3 = st.selectbox("選擇左下地圖的底圖:", basemap_options, index=basemap_options.index("OpenStreetMap"))
-    st.write("#### 左側地圖：水質測站 (聚合點圖)")
+    st.write("#### 左側地圖：水質測站 (Marker Cluster)")
     m3 = leafmap.Map(center=[23.5, 121], zoom=8)
     m3.add_basemap(basemap3)
     if water_quality_stations_gdf is not None:
@@ -74,7 +74,7 @@ with row2_col1:
 
 with row2_col2:
     basemap4 = st.selectbox("選擇右下地圖的底圖:", basemap_options, index=basemap_options.index("OpenStreetMap"))
-    st.write("#### 右側地圖：釣魚點 (聚合點圖)")
+    st.write("#### 右側地圖：釣魚點 (Marker Cluster)")
     m4 = leafmap.Map(center=[23.5, 121], zoom=8)
     m4.add_basemap(basemap4)
     if fishing_spots_gdf is not None:
