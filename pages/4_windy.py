1ef2ed10482673862357152f056a870beed87a93
Bryan77778 <s1143018@gm.ncue.edu.tw>
2024-12-17 18:55:04 +0800

    Update 4_windy.py

--- pages/4_windy.py
+++ pages/4_windy.py
@@ -3,28 +3,35 @@ import leafmap.foliumap as leafmap
 
 # GeoJSON 資料 URL
 water_quality_stations_url = "https://github.com/Bryan77778/11-27/raw/refs/heads/main/%E6%B5%B7%E5%9F%9F%E6%B0%B4%E8%B3%AA%E6%B8%AC%E7%AB%99.geojson"
-fishing_spots_url = "https://github.com/Bryan77778/11-27/raw/refs/heads/main/%E5%85%A8%E5%8F%B0%E9%96%8B%E6%94%BE%E9%87%A3%E9%BB%9E%E4%BD%8D%E7%BD%AE%20(1).geojson"
 
 # Streamlit 應用標題
-st.title("Windy API 與水質測站點位地圖 Viewer")
-
-# 提供底圖選項
-basemap_options = list(leafmap.basemaps.keys())
+st.title("分割地圖展示：Windy 風速與水質測站點位")
 
 # 第一行地圖顯示
 st.write("### 第一行地圖")
 row1_col1, row1_col2 = st.columns(2)
 
-# 左側地圖 - 顯示水質測站
+# 左側地圖 - Windy 動態風速圖 (iframe)
 with row1_col1:
-    st.write("#### 左側地圖：水質測站點位")
-    basemap1 = st.selectbox("選擇左側地圖的底圖:", basemap_options, index=basemap_options.index("OpenStreetMap"))
-    
+    st.write("#### 左側地圖：Windy 動態風速")
+    windy_embed_code = """
+    <iframe 
+        width="100%" 
+        height="500" 
+        src="https://embed.windy.com/embed2.html?lat=23.5&lon=121&zoom=6&level=surface&overlay=wind&menu=&message=&marker=&calendar=&pressure=&type=map&location=coordinates&detail=&detailLat=23.5&detailLon=121&metricWind=default&metricTemp=default&radarRange=-1" 
+        frameborder="0">
+    </iframe>
+    """
+    st.components.v1.html(windy_embed_code, height=500)
+
+# 右側地圖 - 水質測站點位
+with row1_col2:
+    st.write("#### 右側地圖：水質測站點位")
     # 創建 Leafmap 地圖
-    m1 = leafmap.Map(center=[23.5, 121], zoom=7)
-    m1.add_basemap(basemap1)
-    m1.add_geojson(water_quality_stations_url, layer_name="Water Quality Stations")
-    m1.to_streamlit(height=500)
+    m = leafmap.Map(center=[23.5, 121], zoom=7)
+    m.add_basemap("OpenStreetMap")
+    m.add_geojson(water_quality_stations_url, layer_name="水質測站點位")
+    m.to_streamlit(height=500)
 
 # 右側地圖 - 整合 Windy API + 水質測站
 with row1_col2:
