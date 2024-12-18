25e28203fc0c3850d65be4c63e86f064f0c5f7b5
Bryan77778 <s1143018@gm.ncue.edu.tw>
2024-12-18 14:51:39 +0800

    Update 4_Windy與氣象資料預測.py

--- pages/4_Windy與氣象資料預測.py
+++ pages/4_Windy與氣象資料預測.py
@@ -8,9 +8,8 @@ from streamlit_folium import st_folium
 WINDY_API_KEY = "Q2V4GyCCzdkfMxBXqrplP2UbxXLjBrEn"
 water_quality_stations_url = "https://github.com/Bryan77778/11-27/raw/refs/heads/main/%E6%B5%B7%E5%9F%9F%E6%B0%B4%E8%B3%AA%E6%B8%AC%E7%AB%99.geojson"
 
-# 天氣與潮汐預報 JSON URL
+# 天氣預報 JSON URL
 weather_forecast_url = "https://github.com/Bryan77778/11-27/raw/refs/heads/main/%E6%B5%B7%E8%B1%A1%E8%B3%87%E6%96%99%E9%A0%90%E5%A0%B1.json"
-tide_forecast_url = "https://github.com/Bryan77778/11-27/raw/refs/heads/main/%E6%BD%AE%E6%B1%90%E9%A0%90%E5%A0%B1-%E6%9C%AA%E4%BE%861%E5%80%8B%E6%9C%88%E6%BD%AE%E6%B1%90%E9%A0%90%E5%A0%B1.json"
 
 # Streamlit 應用標題
 st.title("Windy與氣象資料預測")
@@ -55,38 +54,29 @@ if click_info and click_info.get("last_clicked"):
     st.session_state["windy_zoom"] = 12
     st.experimental_rerun()
 
-# 下方：氣象與潮汐預報
+# 下方：屬性資料表
 st.write("### 台灣周遭海域海象預測(6hr)")
 
+# 解析 JSON 資料
 try:
     weather_data = pd.read_json(weather_forecast_url)
-    tide_forecasts = weather_data["cwaopendata"]["Resources"]["Resource"]["Data"]["TideForecasts"]
+    location_data = weather_data["cwaopendata"]["dataset"]["location"]
 
     # 組織資料表
     table_data = []
-    for forecast in tide_forecasts:
-        location_name = forecast["Location"]["LocationName"]
-        for daily_data in forecast["Location"]["TimePeriods"]["Daily"]:
-            date = daily_data["Date"]
-            lunar_date = daily_data["LunarDate"]
-            tide_range = daily_data["TideRange"]
-            for tide_time in daily_data["Time"]:
-                tide_time_data = {
-                    "地點": location_name,
-                    "日期": date,
-                    "農曆日期": lunar_date,
-                    "潮差": tide_range,
-                    "潮汐": tide_time["Tide"],
-                    "時間": tide_time["DateTime"],
-                    "相對台灣高程系統": tide_time["TideHeights"]["AboveTWVD"],
-                    "相對當地平均海平面": tide_time["TideHeights"]["AboveLocalMSL"],
-                    "相對海圖": tide_time["TideHeights"]["AboveChartDatum"],
-                }
-                table_data.append(tide_time_data)
-
-    # 將資料轉為 Pandas DataFrame 並顯示於 Streamlit
+    for location in location_data:
+        loc_name = location["locationName"]
+        for element in location["weatherElement"]:
+            for time_data in element["time"]:
+                table_data.append({
+                    "地點": loc_name,
+                    "要素": element["elementName"],
+                    "起始時間": time_data["startTime"],
+                    "結束時間": time_data["endTime"],
+                    "描述": time_data["parameter"]["parameterName"],
+                    "數值": time_data["parameter"].get("parameterValue", "N/A")
+                })
     df = pd.DataFrame(table_data)
-    st.write("### 潮汐預報資料")
     st.dataframe(df)
 
 except Exception as e:
