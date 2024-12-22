182fd2e52b65817fdcef542ec24b167bb81214b8
Bryan77778 <s1143018@gm.ncue.edu.tw>
2024-12-22 21:33:41 +0800

    Update 1_Interactive_Map.py

--- pages/1_Interactive_Map.py
+++ pages/1_Interactive_Map.py
@@ -21,10 +21,11 @@ except Exception as e:
 
 # 提供底圖選項
 basemap_options = list(leafmap.basemaps.keys())
+default_basemap = "OpenTopoMap"
 
 # 一、點位地圖呈現
 st.subheader("一、點位地圖呈現")
-selected_basemap = st.selectbox("選擇點位地圖的底圖", basemap_options, index=basemap_options.index("OpenStreetMap"))
+selected_basemap = st.selectbox("選擇點位地圖的底圖", basemap_options, index=basemap_options.index(default_basemap))
 m = leafmap.Map(locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
 m.add_basemap(selected_basemap)
 if county_gdf is not None:
@@ -41,7 +42,7 @@ row1_col1, row1_col2 = st.columns(2)
 
 with row1_col1:
     st.write("### 左側地圖：水質測站點位地圖")
-    basemap1 = st.selectbox("選擇水質測站地圖的底圖", basemap_options, key="map1")
+    basemap1 = st.selectbox("選擇水質測站地圖的底圖", basemap_options, key="map1", index=basemap_options.index(default_basemap))
     m1 = leafmap.Map(center=[23.5, 121], zoom=8)
     m1.add_basemap(basemap1)
     if water_quality_stations_gdf is not None:
@@ -50,7 +51,7 @@ with row1_col1:
 
 with row1_col2:
     st.write("### 右側地圖：釣魚點點位地圖")
-    basemap2 = st.selectbox("選擇釣魚點地圖的底圖", basemap_options, key="map2")
+    basemap2 = st.selectbox("選擇釣魚點地圖的底圖", basemap_options, key="map2", index=basemap_options.index(default_basemap))
     m2 = leafmap.Map(center=[23.5, 121], zoom=8)
     m2.add_basemap(basemap2)
     if fishing_spots_gdf is not None:
@@ -63,7 +64,7 @@ row2_col1, row2_col2 = st.columns(2)
 
 with row2_col1:
     st.write("### 左側地圖：水質測站群集地圖")
-    basemap3 = st.selectbox("選擇水質測站群集地圖的底圖", basemap_options, key="map3")
+    basemap3 = st.selectbox("選擇水質測站群集地圖的底圖", basemap_options, key="map3", index=basemap_options.index(default_basemap))
     m3 = leafmap.Map(center=[23.5, 121], zoom=8)
     m3.add_basemap(basemap3)
     if water_quality_stations_gdf is not None:
@@ -78,7 +79,7 @@ with row2_col1:
 
 with row2_col2:
     st.write("### 右側地圖：釣魚點群集地圖")
-    basemap4 = st.selectbox("選擇釣魚點群集地圖的底圖", basemap_options, key="map4")
+    basemap4 = st.selectbox("選擇釣魚點群集地圖的底圖", basemap_options, key="map4", index=basemap_options.index(default_basemap))
     m4 = leafmap.Map(center=[23.5, 121], zoom=8)
     m4.add_basemap(basemap4)
     if fishing_spots_gdf is not None:
