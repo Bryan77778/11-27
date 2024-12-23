abb746329bbb646627dae8fee175695353a0c609
Bryan77778 <s1143018@gm.ncue.edu.tw>
2024-12-23 11:43:59 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -25,7 +25,6 @@ st.markdown(
         color: white;  /* 設定字體顏色為白色 */
     }
     </style>
-    <div class="stMarkdown">
     資料來自於政府資料開放平台、氣象資料開放平台、Windy，詳細資料如下 \n
     1. 全台開放釣點位置 \n
     2. 海域水質測站 \n
@@ -49,21 +48,6 @@ st.markdown(
         background-size: cover;  /* 讓背景圖像填滿整個視窗 */
         background-position: center;  /* 背景圖像居中 */
         color: white;  /* 設定字體顏色為白色 */
-    }
-     /* 調整背景圖片的透明度 */
-    .stApp::after {
-        content: "";
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background-image: url('https://i.imgur.com/D6zbNRN.png');
-        background-size: cover;
-        background-position: center center;
-        background-attachment: fixed;
-        opacity: 0.3;  /* 調整透明度，0表示完全透明，1表示完全不透明 */
-        z-index: -1;  /* 讓背景圖片在內容下面 */
     }
     </style>
     """,
