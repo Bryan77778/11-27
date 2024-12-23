3ce7a75263e1dc6ea67867f5398bf95213c7117f
Bryan77778 <s1143018@gm.ncue.edu.tw>
2024-12-23 11:56:19 +0800

    Update 3_台灣周遭海域水質狀況.py

--- pages/3_台灣周遭海域水質狀況.py
+++ pages/3_台灣周遭海域水質狀況.py
@@ -7,19 +7,48 @@ import numpy as np
 # 設定 Streamlit 頁面配置
 st.set_page_config(layout="wide")
 st.title("台灣周遭海域水質狀況")
+
 st.markdown(
-"""
-- 化學元素會以熱點圖的形式呈現，分別有重金屬、有機化合物、懸浮物質
-- 圖台右上方可勾選以呈現不同的化學元素
-- 屬性資料表展示化學元素數值
-- 當測站中的元素數值大於90%時，會被視為離群測站
-- 某個化學元素離群測站超過五個會被繪製成直方圖
-- 可用於參考海域水質情況
-- **重金屬**：Cd 鎘, Cr 鉻, Cu 銅, Zn 鋅, Pb 鉛, Hg 汞
-- **有機化合物**：NO3_N 硝酸氮, MI3PO4 磷酸鹽, NO2_N 亞硝酸氮, SiO2 二氧化矽
-- **懸浮固體**：SS 二氧化矽
-"""
+    """
+    <style>
+    .stApp {
+        background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), 
+                          url('https://i.imgur.com/D6zbNRN.png'); /* 添加半透明黑色遮罩 */
+        background-size: cover; /* 背景圖片填滿窗口 */
+        background-position: center; /* 背景圖片居中 */
+        color: white; /* 全局字體顏色設置為白色 */
+    }
+    .custom-text {
+        font-size: 18px; /* 設置文字大小 */
+        line-height: 1.6; /* 行距 */
+        color: white; /* 設置文字顏色 */
+    }
+    .stSelectbox div[role="combobox"] {
+        color: white;  /* 下拉選單內的文字設置為黑色（避免與白色背景衝突） */
+        background-color: white;  /* 下拉選單背景設置為白色 */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
 )
+
+st.markdown(
+    """
+    <div class="custom-text">
+    - 化學元素會以熱點圖的形式呈現，分別有重金屬、有機化合物、懸浮物質
+    - 圖台右上方可勾選以呈現不同的化學元素
+    - 屬性資料表展示化學元素數值
+    - 當測站中的元素數值大於90%時，會被視為離群測站
+    - 某個化學元素離群測站超過五個會被繪製成直方圖
+    - 可用於參考海域水質情況
+    - **重金屬**：Cd 鎘, Cr 鉻, Cu 銅, Zn 鋅, Pb 鉛, Hg 汞
+    - **有機化合物**：NO3_N 硝酸氮, MI3PO4 磷酸鹽, NO2_N 亞硝酸氮, SiO2 二氧化矽
+    - **懸浮固體**：SS 二氧化矽
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
 # 側邊欄內容
 markdown = """
 Web App URL: <https://geotemplate.streamlit.app>
