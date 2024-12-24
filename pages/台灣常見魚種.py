a41c8b469bd5118213c76cbf8c897248e1dd40fa
Bryan77778 <s1143018@gm.ncue.edu.tw>
2024-12-24 16:39:10 +0800

    Update 台灣常見魚種.py

--- pages/台灣常見魚種.py
+++ pages/台灣常見魚種.py
@@ -1,15 +1,18 @@
 import streamlit as st
 
+# 標題
+st.title("台灣常見魚種")
+
 # 圖片網址
 image_urls = [
     "https://i.imgur.com/dwcaKvQ.jpeg",
     "https://i.imgur.com/VlARaQ4.jpeg"
 ]
 
-# 創建分頁
-tabs = st.tabs(["Image 1", "Image 2"])
+# 創建單一分頁
+tab = st.tabs(["魚種圖片展示"])[0]
 
-# 在每個分頁中顯示圖片
-for tab, image_url in zip(tabs, image_urls):
-    with tab:
+# 在分頁中上下排列顯示圖片
+with tab:
+    for image_url in image_urls:
         st.image(image_url, caption=image_url, use_column_width=True)
